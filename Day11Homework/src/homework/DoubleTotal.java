00e2257962458ace0b883dc7891f0a3721329fad
Gretapro <96381233+Gretapro@users.noreply.github.com>
2022-01-05 23:48:46 -0500

    Update DoubleTotal.java

--- Day11Homework/src/homework/DoubleTotal.java
+++ Day11Homework/src/homework/DoubleTotal.java
@@ -4,18 +4,18 @@ public class DoubleTotal {
 
 	public static void main(String[] args) {
 
-		int result = sumDouble(2, 3);
-		System.out.println(result);
+		int  myTotal = sumDouble(2, 3);
+		System.out.println(myTotal);
 	}
 
 	public static int sumDouble(int valueOne, int valueTwo) {
 
 		int result = valueOne + valueTwo;
 		if (valueOne == valueTwo) {
-			result = result * 2;
+			myTotal = myTotal * 2;
 
 		}
-		return result;
+		return myTotal;
 	}
 
 }
